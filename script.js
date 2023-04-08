b3a1ad65541ebffb88e72c9d7b1e95d232cdd695
Bekam <mspbekan@gmail.com>
2023-04-07 23:38:58 -0500

    code for storing user's data added

--- script.js
+++ script.js
@@ -8,38 +8,51 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  var saveButton = $('.saveBtn')
+  // var userInput = $('.description')
+  var timeBlockEl = $('.time-block')
+  var hourBlock = $('.hour')
+
+
   
-
-// var divEl = $('time-block')
-//   var textAreaEl = $('.description') 
-  
- var currentDayEl = $('#currentDay');
-  var timeNow = dayjs().format('dddd, MMMM DD')
-
-  currentDayEl.text(timeNow);
-
-var timeBlockEl = $('.time-block')
+  // console.log(userInput)
+  // console.log(hourBlock)
 
 
+  saveButton.on('click', function(e){
+    // 
 
-
-
-var hourBlock = $('.hour')
-
-
-for(var i =0; i<timeBlockEl.length; i++){
-  if(hourBlock.eq(i).text() == dayjs().format('ha')){
+    var eventDescription = $(this).siblings('.description');
+    var userActualValue = eventDescription.val();
+    var uniqueId = eventDescription.parent().attr('id');
     
-    timeBlockEl.eq(i).removeClass('past future')
-    timeBlockEl.eq(i).addClass('present')
-  } else if(hourBlock.eq(i).text() < dayjs().format('ha')){
-    timeBlockEl.eq(i).removeClass('present future')
-    timeBlockEl.eq(i).addClass('past')
-  }else{
-    timeBlockEl.eq(i).removeClass('present past')
-    timeBlockEl.eq(i).addClass('future')
-  }
-}
+    var storeUserData = JSON.parse(localStorage.getItem("user event"))  || [];
+          //  or???? this?     var storeUserData = JSON.parse(localStorage.getItem(unique))  || [];
+   
+          //this logic has no effect on the submission of empty user value ??why not
+          if (userActualValue.trim() === '' || storeUserData.includes(userActualValue)){
+        e.preventDefault(); 
+        return;
+       
+    }
+     
+
+    console.log(eventDescription.val())
+
+      storeUserData.push({id:uniqueId, userActualValue})
+  
+     
+  localStorage.setItem("user event",JSON.stringify(storeUserData))  
+// similarly .. localStorage.setItem(unique,JSON.stringify(storeUserData)) 
+
+ })
+
+
+
+
+
+ 
 
 
 
@@ -53,7 +66,20 @@ for(var i =0; i<timeBlockEl.length; i++){
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time? ??? 24-hour format???????????????????????????????????????????????????
 
+ //?? TimeBlock in the '#hour-9' takes the future class. why not changing??
+  for(var i =0; i<timeBlockEl.length; i++){
+    if(hourBlock.eq(i).text() -(1) ===dayjs().format('hA')){
 
+      timeBlockEl.eq(i).removeClass('past future')
+      timeBlockEl.eq(i).addClass('present')
+    } else if(hourBlock.eq(i).text() -(1)< dayjs().format('hA')){  //convert text to milliseconds
+      timeBlockEl.eq(i).removeClass('present future')
+      timeBlockEl.eq(i).addClass('past')
+    }else{
+      timeBlockEl.eq(i).removeClass('present past')
+      timeBlockEl.eq(i).addClass('future')
+    }
+  }
 
 
   //
@@ -63,6 +89,9 @@ for(var i =0; i<timeBlockEl.length; i++){
   //
   // TODO: Add code to display the current date in the header of the page.
 
-  
+  var currentDayEl = $('#currentDay');
+  var timeNow = dayjs().format('dddd, MMMM DD')
+
+  currentDayEl.text(timeNow);
    
 });
